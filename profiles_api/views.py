83581f6c56b26ed2931501fdb139521e3b4be59e
Ching Miao Lin <miao100tw@gmail.com>
2020-04-06 21:41:49 +0800

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication # 當user log in時 隨機產生token給他，以便之後需要做authentication時可以直接用這個token判斷
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -104,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
